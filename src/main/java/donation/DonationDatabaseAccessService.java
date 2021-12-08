7ef91bf3fd6cd304183547beea29a4b881b804e3
laiba9999 <laibakirmani@hotmail.com>
2021-12-08 14:30:24 +0000

    added donation. There is a bug with get donation by user

--- src/main/java/donation/DonationDatabaseAccessService.java
+++ src/main/java/donation/DonationDatabaseAccessService.java
@@ -1,31 +1,78 @@
 package donation;
 
-import org.flywaydb.core.internal.jdbc.JdbcTemplate;
 import org.flywaydb.core.internal.jdbc.RowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import request.Request;
+import request.RequestRowMapper;
 import user.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class DonationDatabaseAccessService {
-    private JdbcTemplate jdbcTemplate;
+public class DonationDatabaseAccessService implements DonationDAO {
+    private org.springframework.jdbc.core.JdbcTemplate jdbcTemplate;
 
-    public DonationDatabaseAccessService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public DonationDatabaseAccessService(JdbcTemplate jdbcTemplate) {this.jdbcTemplate = jdbcTemplate;}
+
+    @Override
+    public List<Donation> selectAllDonations() {
+        String sql = """
+                SELECT *
+                FROM donations;
+                """;
+
+        return jdbcTemplate.query(sql, new DonationRowMapper());
     }
 
-    RowMapper rowMapper = new RowMapper() {
-        @Override
-        public Object mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Donation donation = new Donation(
-                    rs.getInt("id"),
-                    rs.getInt("user_id"),
-                    rs.getInt("donation_quantity"),
-                    rs.getInt("donation_quantity"),
-                    rs.getString("request_id"));
-            return donation;
-        }
-    };
+    @Override
+    public Optional<Donation> selectDonationById(Integer id) {
+        String sql = """
+                SELECT *
+                FROM donations
+                WHERE id = ?;
+                """;
+        return jdbcTemplate.query(sql, new DonationRowMapper(), id)
+                .stream()
+                .findFirst();
+    }
+
+    @Override
+    public int AddDonation(Donation donation) {
+        String sql = """
+                INSERT INTO donations (user_id, donation_quantity, request_id)
+                VALUES (?, ?, ?);
+                """;
+        return jdbcTemplate.update(
+                sql,
+                donation.getUser_id(),
+                donation.getDonation_quantity(),
+                donation.getRequest_id());
+    }
+
+    @Override//y
+    public int deleteDonation(int id) {
+        String sql = """
+                DELETE FROM donations
+                WHERE id = ?;
+                """;
+        return jdbcTemplate.update(sql, id);
+    }
+
+    @Override//y
+    public int updateDonation(Integer id, Donation donation) {
+        String sql = """
+                UPDATE requests
+                SET user_id = ?, donation_quantity = ?, request_id = ?
+                WHERE id = ?;
+                """;
+        return jdbcTemplate.update(
+                sql,
+                donation.getUser_id(),
+                donation.getDonation_quantity(),
+                donation.getRequest_id());
+    }
 }
