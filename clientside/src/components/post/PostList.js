a7fe2334f366bad82f296528398ed4b375a2d9bd
Jessica Morse <jess0morse99@gmail.com>
2021-12-11 15:34:22 +0000

    added post and postList

--- clientside/src/components/post/PostList.js
+++ clientside/src/components/post/PostList.js
@@ -1,7 +1,24 @@
+import Post from "./Post"
 
-const PostList = () => {
+const PostList = ({posts}) => {
+
+
+    const postComponents = posts.map(post => {
+        return (
+            <Post
+            id = {post.id}
+            hub_id={post.hub_id}
+            post_body={post.post_body}
+
+             />
+        )})
+
 
     return(
-        
+        <div className="postList">
+        {postComponents}
+        <p>hello world: postList</p>
+        </div>
     )
 }
+export default PostList;
--- clientside/src/pages/Homepage.js
+++ clientside/src/pages/Homepage.js
@@ -1,1 +1,41 @@
-const Homepage
+import PostList from '../components/post/PostList';
+import {useState, useEffect} from "react"
+import {getAllPosts} from "../Adapters/backendAdapter"
+import Post from '../components/post/Post';
+
+const Homepage = () => {
+
+    const [posts, setPosts] = useState(null);
+
+  useEffect(()=>{
+    getAllPosts().then(response =>setPosts(response))
+  },[]);
+
+
+  if(posts){
+    return (
+        <div className="App">
+          <header className="App-header">
+           hello world
+           
+                <PostList posts={posts}/>
+        
+          
+          </header>
+        </div>
+      );
+   }
+    else{
+        return (
+            <div className="App">
+              <header className="App-header">
+               uh oh, post list isn't loading in the homepage
+              
+              </header>
+            </div>
+          );
+        }
+
+    }
+
+export default Homepage;
