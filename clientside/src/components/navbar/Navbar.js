3c1336bbc9780150af0496baee96f2af5ff758b0
laiba9999 <laibakirmani@hotmail.com>
2021-12-15 15:12:25 +0000

    changed the signup button to a profile icon and added hover functionality to the icons

--- clientside/src/components/navbar/Navbar.js
+++ clientside/src/components/navbar/Navbar.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom'
 import {FaBars} from 'react-icons/fa'
 
 export const Nav = styled.nav`
-background: #000;
+background: #FFD541;
 height: 80px;
 display: flex;
 justify-content: space-between;
@@ -49,31 +49,31 @@ margin-right: -24px;
 }
 `
 
-export const NavBtn = styled.nav `
-display: flex;
-align-items: center;
-margin-right: 24px;
+// export const NavBtn = styled.nav `
+// display: flex;
+// align-items: center;
+// margin-right: 24px;
 
-@media screen and (max-width: 768px) {
-    display: none;
-}
-`
+// @media screen and (max-width: 768px) {
+//     display: none;
+// }
+// `
 
-export const NavBtnLink = styled(Link)`
-border-radius: 4px;
-background: #256ce1;
-padding: 10px 22px;
-color: #fff;
-border: none;
-outline: none;
-cursor: pointer;
-transition: all 0.2s ease-in-out;
-text-decoration: none;
+// export const NavBtnLink = styled(Link)`
+// border-radius: 4px;
+// background: #256ce1;
+// padding: 10px 22px;
+// color: #fff;
+// border: none;
+// outline: none;
+// cursor: pointer;
+// transition: all 0.2s ease-in-out;
+// text-decoration: none;
 
-&:hover {
-    transition: all 0.2s ease-in-out;
-    background: #fff;
-    color: #010606;
-}
+// &:hover {
+//     transition: all 0.2s ease-in-out;
+//     background: #fff;
+//     color: #010606;
+// }
 
-`
+// `
