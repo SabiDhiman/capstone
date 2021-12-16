1bd0e2743c1fd552d5fb3eae4df1909e69b46138
laiba9999 <laibakirmani@hotmail.com>
2021-12-16 17:18:41 +0000

    merged navbar with side bar

--- clientside/src/components/sidebar/Navbar.js
+++ clientside/src/components/sidebar/Navbar.js
@@ -9,6 +9,7 @@ display: flex;
 justify-content: space-between;
 padding: 0rem;
 z-index: 10;
+width: 100%;
 `
 export const NavLink = styled(Link)`
 color: #fff;
@@ -48,32 +49,3 @@ margin-right: -24px;
     display: none;
 }
 `
-
-// export const NavBtn = styled.nav `
-// display: flex;
-// align-items: center;
-// margin-right: 24px;
-
-// @media screen and (max-width: 768px) {
-//     display: none;
-// }
-// `
-
-// export const NavBtnLink = styled(Link)`
-// border-radius: 4px;
-// background: #256ce1;
-// padding: 10px 22px;
-// color: #fff;
-// border: none;
-// outline: none;
-// cursor: pointer;
-// transition: all 0.2s ease-in-out;
-// text-decoration: none;
-
-// &:hover {
-//     transition: all 0.2s ease-in-out;
-//     background: #fff;
-//     color: #010606;
-// }
-
-// `
