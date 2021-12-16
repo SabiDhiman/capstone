baec32ac6767265d10c7be0b1e0ab1c850915078
Jessica Morse <jess0morse99@gmail.com>
2021-12-16 09:58:34 +0000

    fixed navigate to donationPage

--- clientside/src/components/navbar/index.js
+++ clientside/src/components/navbar/index.js
@@ -31,7 +31,7 @@ const Navbar = () => {
                 </NavLink>
 
             </NavMenu>
-            <NavLink to='/Donation' activeStyle>
+            <NavLink to='/DonationPage' activeStyle>
                 <img src={profile} className="profile-icon" alt="notification-icon"/>
                 </NavLink>
         </Nav>
